8ef3991126047f66ab02d7bbf4152bdd16eadf16
Marco <mbarozzi@gmail.com>
2024-03-07 06:13:23 -0300

    limpeza de codigo

--- MyApp_!/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-//import OlaMundo from "./src/componens/OlaMundo";
-import PizzaTranslator from "./src/componens/Pizza";
-
-function App() {
-  return  <PizzaTranslator/>;
-}
-
-export default App;
